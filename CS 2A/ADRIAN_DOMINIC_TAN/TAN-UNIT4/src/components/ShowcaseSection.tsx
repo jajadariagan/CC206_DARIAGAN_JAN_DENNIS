cb4a4228a997c3ff1c13f8139861661b34b7bf8e
jajadariagan <jandennis.dariagan@wvsu.edu.ph>
2026-03-17 22:36:51 +0800

    .
    .

--- CS 2A/ADRIAN_DOMINIC_TAN/TAN-UNIT4/src/components/ShowcaseSection.tsx
+++
@@ -1,9 +0,0 @@
-const ShowcaseSection = () => {
-    return (
-        <div className="bg-[var(--card)] w-[calc(50%-12px)] h-32 rounded-lg shadow-[0_4px_12px_rgba(0,0,0,0.1)] flex items-center justify-center">
-            
-        </div>
-    );
-};
-
-export default ShowcaseSection;
